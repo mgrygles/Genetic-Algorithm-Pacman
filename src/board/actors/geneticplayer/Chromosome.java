eee50f7bd2746e0efa0545db582f3751c7c55e80
Andrew Hanes <ahanes@csh.rit.edu>
2015-03-22 22:14:07 -0400

    back to tree

--- src/board/actors/geneticplayer/Chromosome.java
+++ src/board/actors/geneticplayer/Chromosome.java
@@ -3,6 +3,7 @@ package board.actors.geneticplayer;
 import board.Board;
 import board.tiles.BoardNode;
 
+import java.io.Serializable;
 import java.util.*;
 import java.util.function.Predicate;
 
@@ -12,50 +13,26 @@ import java.util.function.Predicate;
 public class Chromosome {
     private List<Predicate<GeneticAlgorithmPlayer>> predicates = new ArrayList<Predicate<GeneticAlgorithmPlayer>>();
     private List<MoveLambda> terminals = new LinkedList<MoveLambda>();
-
-
-    private static List<Predicate<GeneticAlgorithmPlayer>> all_predicates = new ArrayList<Predicate<GeneticAlgorithmPlayer>>();
-    private static List<MoveLambda> all_terminals = new LinkedList<MoveLambda>();
-
-    private static final HashMap<Predicate<GeneticAlgorithmPlayer>, String> predicateToStr = new HashMap<>();
-    private static final HashMap<MoveLambda, String> actionToStr = new HashMap<>();
+    private HashMap<Predicate<GeneticAlgorithmPlayer>, String> predicateToStr = new HashMap<>();
+    private HashMap<MoveLambda, String> actionToStr = new HashMap<>();
+    private MoveTree mTree;
     public double score = 0;
-    private int border = Board.rng.nextInt(35);
-
-    public BoardNode move(GeneticAlgorithmPlayer player) {
-        int i = 0;
-        for(Predicate<GeneticAlgorithmPlayer> p : this.predicates) {
-            if(p.test(player)) {
-                if(Board.Debug)
-                    System.out.println(Chromosome.predicateToStr.get(p));
-                if(Board.Debug)
-                    System.out.println(this.actionToStr.get(this.terminals.get(i)));
-                return this.terminals.get(i).move(player);
-            }
-            ++i;
-        }
-        return this.terminals.get(this.predicates.size()).move(player);
-    }
-
-    interface MoveLambda {
-        BoardNode move(GeneticAlgorithmPlayer p);
-    }
+    private int border = Board.rng.nextInt();
 
     public Chromosome() {
         HashSet<Object> used = new HashSet<Object>();
-        predicates.add((p) -> (p.nearestGhost(p.getLocation()) > this.border));
-        predicates.add((p) -> (p.nearestEnergizer(p.getLocation()) > this.border));
+        predicates.add((p) -> (p.nearestGhost(p.getLocation()) > border));
+        predicates.add((p) -> (p.nearestEnergizer(p.getLocation()) > border));
         predicates.add((p) -> (p.hasEnergizer(p.getLocation())));
         predicates.add((p) -> (p.inDanger()));
         predicates.add((p) -> (p.invulnerable()));
-        predicates.add((p) -> (p.nearestEnergizerIsSafe(p.getLocation(), this.border)));
+        predicates.add((p) -> (p.nearestEnergizerIsSafe(p.getLocation(), border)));
         predicateToStr.put(predicates.get(0), "Nearest Ghost");
         predicateToStr.put(predicates.get(1), "Nearest Energizer");
         predicateToStr.put(predicates.get(2), "Has Energizer");
         predicateToStr.put(predicates.get(3), "In Danger");
         predicateToStr.put(predicates.get(4), "Invulnerable");
         predicateToStr.put(predicates.get(5), "Nearest Energizer Safe");
-
         /*
         List<Predicate<GeneticAlgorithmPlayer>> not_predicates = new ArrayList<Predicate<GeneticAlgorithmPlayer>>();
         for(Predicate<GeneticAlgorithmPlayer> p : predicates) {
@@ -76,98 +53,124 @@ public class Chromosome {
         actionToStr.put(terminals.get(1), "attack");
         actionToStr.put(terminals.get(2), "chain");
         actionToStr.put(terminals.get(3), "energizer");
-
-        if(Chromosome.all_predicates.size() == 0) {
-            Chromosome.all_predicates.addAll(this.predicates);
-            Chromosome.all_terminals.addAll(this.terminals);
-        }
-
         Collections.shuffle(terminals, Board.rng);
-        Collections.shuffle(predicates, Board.rng);
-        List<MoveLambda> terminals = new LinkedList<MoveLambda>();
-        while(terminals.size() != predicates.size() + 1) {
-            terminals.add(this.terminals.get(Board.rng.nextInt(this.terminals.size())));
-        }
-        this.terminals = terminals;
+        this.mTree = this.buildMoveTree(5);
     }
 
     public Chromosome(Chromosome a, Chromosome b) {
-        List<MoveLambda> t1 = new LinkedList<MoveLambda>();
-        List<MoveLambda> t2 = new LinkedList<MoveLambda>();
-        List<Predicate<GeneticAlgorithmPlayer>> p1 = new ArrayList<Predicate<GeneticAlgorithmPlayer>>();
-        List<Predicate<GeneticAlgorithmPlayer>> p2 = new ArrayList<Predicate<GeneticAlgorithmPlayer>>();
-        if(Board.rng.nextBoolean()) {
-            //Take from first
-            this.border = a.border;
-            t1 = a.terminals.subList(0, a.terminals.size()/2);
-            t2 = b.terminals.subList(a.terminals.size()/2, a.terminals.size());
-            this.terminals.addAll(t1);
-            this.terminals.addAll(t2);
+        this.border = (Board.rng.nextBoolean() ?  a.border : b.border);
+        this.mTree = merge(a.mTree, b.mTree);
+    }
 
-            this.predicates.addAll(a.predicates.subList(0, a.predicates.size()/2));
-            this.predicates.addAll(b.predicates.subList(b.predicates.size() / 2, b.predicates.size()));
+    private MoveTree buildMoveTree(int size) {
+        if (size == 0) {
+            int index = Board.rng.nextInt(terminals.size());
+            return new MoveTreeLeaf(terminals.get(index), this.actionToStr.get(terminals.get(index)));
+        } else {
+            int index = Board.rng.nextInt(predicates.size());
+            Predicate<GeneticAlgorithmPlayer> p = this.predicates.get(index);
+            String s = this.predicateToStr.get(p);
+            return new MoveTreePredicate(p, buildMoveTree(size - 1), buildMoveTree(size - 1), s);
         }
-        else {
-            //Take from second
-            this.border = b.border;
-            t2 = a.terminals.subList(0, a.terminals.size()/2);
-            t1 = b.terminals.subList(a.terminals.size()/2, a.terminals.size());
-            this.terminals.addAll(t1);
-            this.terminals.addAll(t2);
-            this.predicates.addAll(b.predicates.subList(0, b.predicates.size() / 2));
-            this.predicates.addAll(b.predicates.subList(a.predicates.size() / 2, a.predicates.size()));
-        }
+    }
+
+    public BoardNode move(GeneticAlgorithmPlayer p) {
+        return this.mTree.eval(p);
     }
 
     public void mutate() {
-        if(Board.rng.nextFloat() > .50) {
-            // Two choices
-            if(Board.rng.nextBoolean()) {
-                int a, b;
-                a = Board.rng.nextInt(this.predicates.size());
-                b = Board.rng.nextInt(this.predicates.size());
-                Predicate<GeneticAlgorithmPlayer> p = this.predicates.get(a);
-                this.predicates.set(a, this.predicates.get(b));
-                this.predicates.set(b, p);
-            }
-            else {
-                if(Board.rng.nextBoolean()) {
-                    // Mutate predicate
-                    this.predicates.set(Board.rng.nextInt(this.predicates.size()), Chromosome.all_predicates.get(Board.rng.nextInt(Chromosome.all_predicates.size())));
-                } else {
-                    //Mutate terminal
-                    this.terminals.set(Board.rng.nextInt(this.terminals.size()), Chromosome.all_terminals.get(Board.rng.nextInt(Chromosome.all_terminals.size())));
-                }
-            }
-        }
-        if(Board.rng.nextFloat() > .5) {
-            if(Board.rng.nextBoolean()) {
-                this.border --;
-            } else {
-                this.border++;
-            }
-        }
-        if(Board.rng.nextFloat() > .5 && false) {
-            int index = Board.rng.nextInt(this.predicates.size());
-            int index2 = Board.rng.nextInt(this.all_predicates.size());
-            String s1, s2;
-            s1 = Chromosome.predicateToStr.get(this.predicates.get(index));
-            s2 = Chromosome.predicateToStr.get(this.all_predicates.get(index2));
-            Predicate<GeneticAlgorithmPlayer> combo = this.predicates.get(index).and(this.all_predicates.get(index2));
-            this.predicates.set(index, combo);
-            Chromosome.predicateToStr.put(combo, s1 + " && " + s2);
+        if(Board.rng.nextFloat() > .95) {
+            Chromosome g = new Chromosome(this, new Chromosome());
+            this.mTree = g.mTree;
         }
     }
 
+    private MoveTree merge(MoveTree a, MoveTree b) {
+        MoveTree n;
+        if (Board.rng.nextBoolean()) {
+            n = copy(a);
+        } else {
+            n = copy(b);
+        }
+        MoveTreePredicate foo = ((MoveTreePredicate) n);
+        MoveTreePredicate foo1 = ((MoveTreePredicate) a);
+        MoveTreePredicate foo2 = ((MoveTreePredicate) b);
+        if (Board.rng.nextBoolean()) {
+            foo.l = copy(foo1.l);
+            foo.r = copy(foo2.r);
+        } else {
+            foo.r = copy(foo1.r);
+            foo.l = copy(foo1.l);
+        }
+        return n;
+    }
+
+
     public String toString() {
-        String s = "Border: %s\nPredicates:\n";
-        for(Predicate<GeneticAlgorithmPlayer> p : this.predicates) {
-            s += "\t"+Chromosome.predicateToStr.get(p) + "\n";
+        return this.mTree.toString();
+    }
+
+    private MoveTree copy(MoveTree a) {
+        if (a instanceof MoveTreePredicate) {
+            MoveTreePredicate a2 = (MoveTreePredicate) a;
+            return new MoveTreePredicate(a2.t, copy(a2.l), copy(a2.r), a2.n);
+        } else { //Terminal
+            MoveTreeLeaf a2 = (MoveTreeLeaf) a;
+            return new MoveTreeLeaf(a2.m, a2.n);
         }
-        s += "Terminals:\n";
-        for(MoveLambda m : this.terminals) {
-            s += "\t"+Chromosome.actionToStr.get(m) + "\n";
+    }
+
+    interface MoveLambda {
+        BoardNode move(GeneticAlgorithmPlayer p);
+    }
+
+    interface MoveTree {
+        BoardNode eval(GeneticAlgorithmPlayer player);
+    }
+
+    private class MoveTreePredicate implements MoveTree {
+        Predicate<GeneticAlgorithmPlayer> t;
+        String n;
+        MoveTree l, r;
+
+        public MoveTreePredicate(Predicate<GeneticAlgorithmPlayer> t, MoveTree l, MoveTree r, String n) {
+            this.t = t;
+            this.n = n;
+            this.l = l;
+            this.r = r;
         }
-        return String.format(s, this.border);
+
+        @Override
+        public BoardNode eval(GeneticAlgorithmPlayer player) {
+            if (Board.Debug)
+                System.out.println("Called " + this.n);
+            return this.t.test(player) ? r.eval(player) : l.eval(player);
+        }
+
+        public String toString() {
+            return String.format("( %s %s %s )", this.n, this.l.toString(), this.r.toString());
+        }
+    }
+
+    private class MoveTreeLeaf implements MoveTree {
+        MoveLambda m;
+        String n;
+
+        public MoveTreeLeaf(MoveLambda m, String n) {
+            this.m = m;
+            this.n = n;
+        }
+
+        public BoardNode eval(GeneticAlgorithmPlayer player) {
+            if (Board.Debug)
+                System.out.println("Called " + this.n + "\n");
+            return this.m.move(player);
+        }
+
+        public String toString() {
+            return String.format("( %s )", this.n);
+        }
+
+
     }
 }
